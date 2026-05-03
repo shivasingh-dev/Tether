9c3766e5cf6601902767938d33946b1993c838c1
Shiva Singh <shivasingh247044@gmail.com>
2026-05-03 20:49:32 +0530

    dd

--- Backend/config/emailConfig.js
+++ Backend/config/emailConfig.js
@@ -2,8 +2,6 @@ import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 dotenv.config()
 
-import nodemailer from "nodemailer";
-
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
